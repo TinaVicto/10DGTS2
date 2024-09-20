07df07812114d53845200d1ec9d68f9f153d11ce
TinaVicto <veronica.choi@kingsway.school.nz>
2024-09-20 14:38:09 +1200

    Add files via upload
    My first and second projects of python

---
+++ Variable.py
@@ -0,0 +1,26 @@
+# showing how variables work 
+# Author: Veronica Choi
+# Date: 20 September 2024
+# Version:1 
+
+
+# different types of variables
+# Store a number
+num_1 = 60 # assigning 60 to num_1
+print (num_1) # This displays what num_1 contains 
+
+# Store a string
+name = "Veroncia"
+print (name)
+name = "heheheh" # reassinging value to an existint one
+print(name)
+
+# Do calculations in a variable and store the result
+sum = 5 + 5 # good practice need space 
+print (sum)
+sum = "5 + 5 = 10" # this is a string 
+print(sum) 
+
+# Adding strings together is called concatenation 
+concatenation = num_1 + sum_string
+print(concatenation)
---
+++ Welcome.py
@@ -0,0 +1,10 @@
+# My First Python program
+# Author: Veroncia Choi
+# Date: 2024-09-18
+# Version: 1.0 
+
+
+# Print out a welcome message. 
+print('Hello 10DGT!\n') #\n creates a line break
+Print()# This creates a line break
+print('Congratulations in writing your first program')
