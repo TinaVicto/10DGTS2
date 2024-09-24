e591d06c1efce87cac32d8c526f4f4f8fd84f7a0
TinaVicto <veronica.choi@kingsway.school.nz>
2024-09-25 09:58:57 +1200

    Add files via upload
    My learning of Loop and Indents in python

---
+++ Loops and Indents.py
@@ -0,0 +1,22 @@
+# loops and indents
+# Author: Veronica Choi
+# Date: 20 September 2024
+# Version: 1 
+
+
+# For loops. Repeat for a set number of times. 
+# For start the loop
+#next is the name of the loop
+# in range tells the loop how many times to run
+# the number in the () is how many times it repeats. 
+for name_of_loop in range (5):
+    print("ha ")
+
+# while loop. Runs until condition is met or no longer is met.
+keep_going = "" # empty variable 
+while keep_going == "":
+    print("Looping")
+    print("still looping") 
+
+    keep_going = input("Again? or press any other key to quit")
+    
